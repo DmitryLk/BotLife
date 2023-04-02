b48e0cd6004011edfeb090ac23cd5ac6b27fc3fb
Lokosov Dmitry <l_dima@inbox.ru>
2023-04-02 22:04:41 +0300

    yutu

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApp1
 		static void Main()
 		{
 			Application.EnableVisualStyles();
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
 		}
