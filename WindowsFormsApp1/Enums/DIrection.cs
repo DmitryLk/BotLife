58861fdc26ec643571bd1cd7e42ab5ad39a5aa8f
TCSBANK\d.lokosov <d.lokosov@tinkoff.ru>
2023-04-17 19:27:44 +0300

    выа

--- WindowsFormsApp1/Enums/DIrection.cs
+++ WindowsFormsApp1/Enums/DIrection.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml;
 
-namespace WindowsFormsApp1.Enums
-{
-    public enum Direction
-    {
-        Up = 0,
-        UpRight = 1,
-        Right = 2,
-        DownRight = 3,
-        Down = 4,
-        DownLeft = 5,
-        Left = 6,
-        UpLeft = 7
-    }
-}
+//namespace WindowsFormsApp1.Enums
+//{
+//    public enum Direction
+//    {
+//        Up = 0,
+//        UpRight = 1,
+//        Right = 2,
+//        DownRight = 3,
+//        Down = 4,
+//        DownLeft = 5,
+//        Left = 6,
+//        UpLeft = 7
+//    }
+//}
